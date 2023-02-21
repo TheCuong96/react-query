0ec388d548b436bd8c32e3e31d61b4f6d9f3e771
thecuong1396 <cuongphan@coinmap.tech>
2023-02-22 04:26:24 +0700

    vòng đời và tính chất staleTime, cacheTime, keepPreviousData giữa các component và bên trong component thông qua queryKey

--- react-query/src/pages/Students/Students.tsx
+++ react-query/src/pages/Students/Students.tsx
@@ -43,7 +43,27 @@ export default function Students() {
 
   const { data, isLoading } = useQuery({
     queryKey: ['students', page], // queryKey này chúng ta có thể hiểu phần tử thứ nhất là 1 key giống như trong localStorage, và nó dùng để định danh cái api mà chúng ta đang tương tác, nhờ vậy nên ReactQueryDevtools mới có thể truy ra và show lên cho ta xem..., còn page ở đây nó giống như param trong useEffect, khi thằng này bị thay đổi thì nó sẽ chạy lại queryFn, và queryFn sẽ là 1 hàm call api thông thường
-    queryFn: () => getStudents(page, LIMIT)
+    queryFn: () => getStudents(page, LIMIT),
+    // khi ta call những data có cùng 1 key ví dụ như ở đây là key student
+    // thì ghi call data ở page 1 nó sẽ có 2 trạng thái là đã stale(cũ) hoặc chưa stale(cũ)
+    // stale(cũ) ở đây mặc định là 0 time, thì khi call xong thường sẽ là stale(cũ) ngay lập tức
+    // thì sự khác nhau giữa đã stale(cũ) và chưa stale(cũ) là:
+    // nếu stale(cũ) thì nó sẽ gọi lại api, nếu chưa stale(cũ) thì nó sẽ không gọi lại api mà nó lấy data trong bộ nhớ cache ngầm để show ra cho ta và nó sẽ call api ngầm,khi call xong thì sẽ update lại data cached và trả về data mới cho mình, và trạng thái fetch sẽ là isLoading: false
+    // ta có thể set thời gian stale(cũ) bằng staleTime: 60 * 1000
+    // staleTime: 60 * 1000,
+    // ở đây useQuery có 1 đặc điểm là nếu những component khác có trùng key với queryKey là students
+    // ví dụ ta có 3 Component A, B, C
+    // khi ta fetch api có key là students ở Component A thì khi ta fetch xong nó sẽ call api bình thường
+    // nhưng khi ta qua Component B và ta fetch api vẫn có key là students thì nó sẽ trả về data cho B ngay lập tức, điều này cũng xảy ra tương tự với Component C bất chấp việc nó đã stale(cũ) hay chưa
+    // và trong thời gian này nó sẽ âm thầm fetch data lại 1 lần nữa và cập nhật hết cho cả 3 Component A, B, C
+    // nhưng khi cả 3 đều bị unmount( nghĩa là bị tắt hoặc ko còn sử dụng tới 3 Component đó nữa) thì nó sẽ đếm ngược sau 5 phút, và xóa data ở bộ nhớ đệm đi, và sau 5 phút nếu 1 trong 3 Component bị gọi lại thì nó sẽ fetch data lại
+    //////////////////////////////
+    // ở trên ta có thời gian cũ, nhưng nó còn có 1 loại thời gian nữa là thời gian lưu dữ data đã cũ(lưu ý là nó không lưu trữ data mới, data mới là data vừa gọi hiện tại)
+    // cacheTime này có đặc điểm là khi nó hết thời gian thì nó sẽ xóa luôn những data đang cũ ra khỏi bộ nhớ ngầm của nó
+    // nếu những data cũ bị xóa khỏi bộ nhớ ngầm, thì khi gọi lại những data đó nó sẽ chạy lại như 1 api mới 1 cách thông thường
+    // thường thì người ta sẽ cài cacheTime lớn hơn staleTime
+    // cacheTime: 5 * 1000 // mặc định thì sẽ không có thời gian cacheTime, hoặc có thể hiểu là vô thời hạn
+    keepPreviousData: true // là giữ lại data trước đó, và vẫn âm thầm call data mới, khi call xong thì cập nhật cho ta data mới, nên isLoading sẽ luôn là flase
   })
   const totalPage = Math.ceil(Number(data?.headers['x-total-count'] || 0) / LIMIT)
   console.log('totalPage', Array(totalPage).fill(0))
