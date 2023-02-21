1074fcfa3e5412504fe6e2d92210f99a664766a9
thecuong1396 <cuongphan@coinmap.tech>
2023-02-22 00:08:32 +0700

    xử lý show pagination và chuyển page 1 cách dễ dàng với url

--- react-query/src/pages/Students/Students.tsx
+++ react-query/src/pages/Students/Students.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { getStudents } from 'apis/students.api'
+import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Students as StudentsType, Student } from 'types/students.type'
 import { useQueryString } from 'utils/utils'
-
+const LIMIT = 10
 const RenderListStudents = (listStudents: Student[]): JSX.Element => {
   return (
     <>
@@ -41,10 +42,11 @@ export default function Students() {
   const page = Number(queryString.page) || 1
 
   const { data, isLoading } = useQuery({
-    queryKey: ['students', page], // queryKey này chúng ta có thể hiểu phần tử thứ nhất là 1 key giống như trong localStorage, và nó dùng để định danh cái api mà chúng ta đang tương tác, nhờ vậy nên ReactQueryDevtools mới có thể truy ra và show lên cho ta xem..., còn page ở đây nó giống như param trong useEffect, khi thằng này bị thay đổi thì nó sẽ chạy lại queryFn, và queryFn sẽ là 1 hàm call api thông thường 
-    queryFn: () => getStudents(page, 10)
+    queryKey: ['students', page], // queryKey này chúng ta có thể hiểu phần tử thứ nhất là 1 key giống như trong localStorage, và nó dùng để định danh cái api mà chúng ta đang tương tác, nhờ vậy nên ReactQueryDevtools mới có thể truy ra và show lên cho ta xem..., còn page ở đây nó giống như param trong useEffect, khi thằng này bị thay đổi thì nó sẽ chạy lại queryFn, và queryFn sẽ là 1 hàm call api thông thường
+    queryFn: () => getStudents(page, LIMIT)
   })
-  console.log('data', data)
+  const totalPage = Math.ceil(Number(data?.headers['x-total-count'] || 0) / LIMIT)
+  console.log('totalPage', Array(totalPage).fill(0))
 
   /** 
   const [listStudents, setListStudents] = useState<StudentsType>([])
@@ -112,25 +114,55 @@ export default function Students() {
           <nav aria-label='Page navigation example'>
             <ul className='inline-flex -space-x-px'>
               <li>
-                <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
-                  Previous
-                </span>
+                {page === 1 ? (
+                  <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 '>
+                    Previous
+                  </span>
+                ) : (
+                  <Link
+                    className='rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 '
+                    to={`/students?page=${page - 1}`}
+                  >
+                    Previous
+                  </Link>
+                )}
               </li>
+              {Array(totalPage)
+                .fill(0)
+                .map((_, index) => {
+                  const pageNumber = index + 1
+                  const isActive = page === pageNumber
+                  // mặc định nó sẽ show page 1 ở lần render đầu tiên, sau đó khi ta click thì sẽ làm thay đổi url có page=giá trị ta vừa click -> khi url thay đổi thì useQueryString sẽ tra ra cho ta  1 page mới, và khi page có sự thay đổi thì useQuery sẽ call lại api và show ra cho ta kết quả như ta mong đợi
+                  return (
+                    <li key={pageNumber}>
+                      <Link
+                        className={classNames(
+                          'border border-gray-300 py-2 px-3 leading-tight hover:bg-gray-100 hover:text-gray-700 ',
+                          {
+                            'bg-gray-100 text-gray-700': isActive,
+                            'bg-white text-gray-500': !isActive
+                          }
+                        )}
+                        to={`/students?page=${pageNumber}`}
+                      >
+                        {pageNumber}
+                      </Link>
+                    </li>
+                  )
+                })}
               <li>
-                <a
-                  className='border border-gray-300 bg-white bg-white py-2 px-3 leading-tight text-gray-500 text-gray-500  hover:bg-gray-100 hover:bg-gray-100 hover:text-gray-700 hover:text-gray-700'
-                  href='/students?page=8'
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
-                  href='/students?page=1'
-                >
-                  Next
-                </a>
+                {page === totalPage ? (
+                  <span className='cursor-not-allowed rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 '>
+                    Next
+                  </span>
+                ) : (
+                  <Link
+                    className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 '
+                    to={`/students?page=${page + 1}`}
+                  >
+                    Next
+                  </Link>
+                )}
               </li>
             </ul>
           </nav>
--- react-query/src/utils/utils.ts
+++ react-query/src/utils/utils.ts
@@ -3,5 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 export const useQueryString = () => {
   const [searchParams] = useSearchParams()
   const searchParamsObject = Object.fromEntries([...searchParams])
+  console.log('searchParams', searchParamsObject) // nó lấy url đằng sau dấu chấm hỏi [http://localhost:3000/students?page=3] --> {page: '3'} và show ra cho ta data mà ta có thể sử dụng được
+
   return searchParamsObject
 }
